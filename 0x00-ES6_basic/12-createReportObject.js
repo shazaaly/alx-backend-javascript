0731ba05d28aee6e1251136fcdbc47884ffd7dad
shazaaly <shaza.aly@gmail.com>
2024-01-02 23:35:36 +0200

    12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+import {createEmployeesObject} from './11-createEmployeesObject'
+export default function createReportObject(employeesList) {
+	const obj = {}
+	obj['allEmployees'] = createEmployeesObject(departmentName, employees)
+	return obj
+
+
+}
