6cd91a432c6a6846deb43e4c8c5e6d7ff8bc8e97
shazaaly <shaza.aly@gmail.com>
2024-01-04 00:08:27 +0200

    get profile data

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,8 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
       const user = data[1]; // createUser() is the second promise, so its result is at index 1
-      console.log(`${user.lastName} ${user.firstName}`);
+      const profile = data[0];
+      console.log(`${profile.body} ${user.lastName} ${user.firstName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
