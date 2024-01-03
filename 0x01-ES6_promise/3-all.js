9d46d6554c773cd4dbad046fb910a4ab2cb9ab67
shazaaly <shaza.aly@gmail.com>
2024-01-04 00:10:16 +0200

    firat name lastname

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
     .then((data) => {
       const user = data[1]; // createUser() is the second promise, so its result is at index 1
       const profile = data[0];
-      console.log(`${profile.body} ${user.lastName} ${user.firstName}`);
+      console.log(`${profile.body} ${user.firstName} ${user.lastName} `);
     })
     .catch(() => console.log('Signup system offline'));
 }
