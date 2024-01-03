bff6b227c2b326918aca3f335b14a3d0fddbbe11
shazaaly <shaza.aly@gmail.com>
2024-01-03 23:41:35 +0200

    handle promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((success) => {
-    const data = {
-      status: 200,
-      body: success,
-
-    };
-    console.log('Got a response from the API');
-    return data;
-  }).catch((error) => error);
+  const data = {
+    status: 200,
+    body: 'success',
+  };
+  return promise
+    .then(() => data)
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
