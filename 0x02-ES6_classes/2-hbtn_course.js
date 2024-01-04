520ade3c9d49dbe79575b64ae826f077c406b541
shazaaly <shaza.aly@gmail.com>
2024-01-04 20:17:54 +0200

    File: 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students || !students.every((std) => typeof std === 'string'))) {
+      throw TypeError('students must be array of Strings');
+    }
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
