5b098d1753c9a65dc414236ff5b0d3f6c0d7e4c7
shazaaly <shaza.aly@gmail.com>
2024-01-07 08:37:40 +0200

    fix2

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,15 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   let res;
+
   try {
     res = mathFunction();
-  } catch (error) {
-    res = error.toString();
+  } catch (err) {
+    res = err.toString();
   }
 
   queue.push(res);
-  queue.push(res, 'Guardrail was processe');
+  queue.push('Guardrail was processed');
 
   return queue;
 }
