96f29d40dcf8ac255bec369c8703828d7afb02eb
shazaaly <shaza.aly@gmail.com>
2024-01-09 01:31:00 +0200

    check set instanceof Set && typeof startString === 'string'

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,15 +5,18 @@ export default function cleanSet(set, startString) {
     return '';
   }
   for (const item of set) {
-    if (item && item.startsWith(startString)) {
-      const strSlice = item.slice(startString.length);
-      if (str !== '') {
-        /* Add hyphen only if it's not the first item, By adding the hyphen
+    if (set instanceof Set && typeof startString === 'string') {
+      if (item && item.startsWith(startString)) {
+        const strSlice = item.slice(startString.length);
+        if (str !== '') {
+          /* Add hyphen only if it's not the first item, By adding the hyphen
 before the item (and only when str is not empty) */
-        str += '-';
+          str += '-';
+        }
+        str += strSlice;
       }
-      str += strSlice;
     }
   }
+
   return str;
 }
