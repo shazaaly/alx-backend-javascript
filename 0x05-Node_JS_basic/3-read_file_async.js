1dd50acd9b61bf59e31e43dad5036bd257b868f5
shazaaly <shaza.aly@gmail.com>
2024-02-29 01:32:30 +0200

    Update function to handle edge case

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim());
+      lines.shift(); // Remove header
+
+      const students = {};
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      });
+
+      console.log(`Number of students: ${lines.length}`); // Log total number of students
+
+      Object.keys(students).forEach((field) => {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      });
+      resolve(data);
+    });
+  });
+}
+
+module.exports = countStudents;
