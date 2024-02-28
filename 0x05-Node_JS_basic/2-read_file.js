438602a3567e8cee9fb2c3205827b8c9418e4552
shazaaly <shaza.aly@gmail.com>
2024-02-28 23:34:30 +0200

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,21 +6,19 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     if (!data) {
-        console.log('Cannot load the database');
-        return
+      console.log('Cannot load the database');
+      return;
     }
     const lines = data.split('\n');
     lines.shift();
     lines.forEach((line) => {
-        if (line.trim() !== ''){
-            const [firstname, , , field] = line.split(',');
-            if (!students[field]) {
-              students[field] = [];
-            }
-            students[field].push(firstname);
-            
+      if (line.trim() !== '') {
+        const [firstname, , , field] = line.split(',');
+        if (!students[field]) {
+          students[field] = [];
         }
-
+        students[field].push(firstname);
+      }
     });
     const fields = Object.keys(students);
     fields.forEach((field) => {
