e668c79c70e8f51bd87c72d3479724f3fc84bdc5
shazaaly <shaza.aly@gmail.com>
2024-02-29 03:12:20 +0200

    Update port number in HTTP server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,38 +1,30 @@
 const http = require('http');
 const countStudents = require('./2-read_file');
 
-
-const port = 1254;
+const port = 1245;
 const host = '127.0.0.1';
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.write('Hello Holberton School!')
+    res.write('Hello Holberton School!');
     res.end('');
-    
   }
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-        const result = countStudents(process.argv[2]); // Assuming the CSV file path is passed as a command-line argument.
-        res.write(result)
-        // Note: countStudents currently logs directly to the console rather than returning data.
-        // You might need to adjust countStudents to return data if you want to include it in the response.
-      } catch (error) {
-        res.write('Cannot load the database');
-      }
-  
-      res.end();
-    
+      const result = countStudents(process.argv[2]);
+      res.write(result);
+    } catch (error) {
+      res.write('Cannot load the database');
+    }
+
+    res.end();
   }
- 
 });
 
 app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`);
-
-
+  console.log(`Server running at http://${host}:${port}/`);
 });
 
 module.exports = app;
