6b831bcf963b3b0329410ea09a6a8bed4bd83d52
shazaaly <shaza.aly@gmail.com>
2024-02-29 03:06:27 +0200

    Refactor countStudents function to improve readability and error handling

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+function countStudents(path) {
   try {
-    const fileContents = fs.readFileSync(fileName, 'utf-8');
-    const lines = fileContents.toString().split('\n');
-    for (let i = 0; i < lines.length; i += 1) {
-      if (lines[i]) {
-        length += 1;
-        const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
-        } else {
-          students[field[3]] = [field[0]];
-        }
-        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
-        } else {
-          fields[field[3]] = 1;
-        }
-      }
+    const data = fs.readFileSync(path, 'utf8');
+    if (!data) {
+      console.log('Cannot load the database');
+      return;
     }
-    const l = length - 1;
-    console.log(`Number of students: ${l}`);
-    for (const [key, value] of Object.entries(fields)) {
-      if (key !== 'field') {
-        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+    const lines = data.split('\n');
+    lines.shift(); // Remove the header line
+
+    const students = {};
+
+    lines.forEach((line) => {
+      if (line.trim() !== '') {
+        const [firstname, , , field] = line.split(',');
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
       }
-    }
-  } catch (error) {
-    throw Error('Cannot load the database');
+    });
+
+    let output = '';
+    const fields = Object.keys(students);
+    fields.forEach((field) => {
+      output += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+    });
+
+    return output;
+
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
 }
 
