84071e34493f97cef1eb15c8dccbd066147ae4c2
shazaaly <shaza.aly@gmail.com>
2024-02-29 14:37:07 +0200

    Refactor code to improve performance and readability

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises;
+
+function readDatabase(filePath) {
+  return fs.readFile(filePath, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim());
+      lines.shift();
+
+      const students = {};
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      });
+
+      let output = `Number of students: ${lines.length}\n`;
+
+      Object.keys(students).forEach((field) => {
+        output += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+      });
+
+      return output;
+    })
+    .catch((err) => {
+      throw Error(err);
+    });
+}
+
+module.exports = readDatabase;
