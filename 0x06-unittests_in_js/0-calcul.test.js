e0a5ce194a4dd980b1d60fce25c7e8cc39e0e8a7
shazaaly <shaza.aly@gmail.com>
2024-03-03 22:22:08 +0200

    Refactor calculateNumber function to use Math.round() and add unit tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,31 @@
 const assert = require("assert");
 const { it, describe } = require("mocha");
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', ()=>{
-    it('correctly sums two numbers', ()=>{
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.2), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.2), 4);
-        assert.strictEqual(calculateNumber(1.7, 3.8), 5);
-        assert.strictEqual(calculateNumber(1, 3), 4);
+describe("calculateNumber()", function() {
 
-
-    })
-
-})
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
+});
